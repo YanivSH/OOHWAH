0f814ff2b51622cb8191ac8a4a9d16cd9a34fd24
Yaniv Marshaly <yaniv@sketchheroes.com>
2013-01-30 15:38:56 +0200

    Initial Commit

---
+++ OOHWAH/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OOHWAH
+//
+//  Created by Yaniv Marshaly on 1/30/13.
+//  Copyright (c) 2013 OOHWAH LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OOHWAH/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OOHWAH
+//
+//  Created by Yaniv Marshaly on 1/30/13.
+//  Copyright (c) 2013 OOHWAH LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
